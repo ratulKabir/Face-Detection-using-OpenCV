aca6048c45d0670874868775fdfbcc4474af59f6
Raihan Kabir Ratul <raihankabirratul@gmail.com>
2018-09-25 17:29:15 +0600

    Update split.py

--- images/split.py
+++ images/split.py
@@ -6,5 +6,5 @@ success = True
 while success:
   success,image = vidcap.read()
   print('Read a new frame: ', success)
-  cv2.imwrite("ratul/frame%d.jpg" % count, image)     # save frame as JPEG file
+  cv2.imwrite("ratul/%d.jpg" % count, image)     # save frame as JPEG file
   count += 1
